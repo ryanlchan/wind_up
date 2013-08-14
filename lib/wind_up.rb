cd8d3bd80d215461c8d41f9506eecdf0ee221bfb
Ryan Chan <ryan@ryanlchan.com>
2013-08-14 00:05:58 -0700

    Add Delegator for true Queue-like functionality
    Delegator exposes the #perform_with method which accepts a delegatee class and
    method arguments. Delegator will create an instance of the delegatee and call
    the #perform method of the delegatee, providing the method arguments.
    
    This allows us to have a single pool of resources which perform multiple
    responsibilities, multiplexing various tasks together.

--- lib/wind_up.rb
+++ lib/wind_up.rb
@@ -7,6 +7,7 @@ require 'wind_up/queue_proxy'
 require 'wind_up/queue_manager'
 require 'wind_up/version'
 require 'wind_up/celluloid_ext'
+require 'wind_up/delegator'
 
 module WindUp
   def self.logger
